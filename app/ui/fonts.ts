4de51deb2610c01ac734512160420c50ba77df2e
ademola adeniji <aademola750@gmail.com>
2024-04-01 13:06:37 +0100

    april1

--- app/ui/fonts.ts
+++
@@ -1,8 +0,0 @@
-import {Inter} from "next/font/google";
-import { Lusitana } from "next/font/google";
-import { Manrope } from "next/font/google";
-
-
-export const inter = Inter({subsets:['latin']})
-export const lusitana = Lusitana({weight:['400', '700'], subsets:['latin']})
-export const manrope = Manrope({weight:['400', '700'], subsets:["latin-ext"]})
